08a960001e85a9d7819b71b4c44ec0d9e55ee258
{kwonjieun} <{>
2023-09-21 01:37:07 +0900

    수정 및 커밋 23/09/13

--- src/main/java/com/css/autocsfinal/config/SecurityConfig.java
+++ src/main/java/com/css/autocsfinal/config/SecurityConfig.java
@@ -106,6 +106,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+//        configuration.setAllowedOrigins(Arrays.asList("http://autocs.coffit.today:3000"));  // 도커용
         configuration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Content-type"
                 , "Access-Control-Allow-Headers", "Authorization"
